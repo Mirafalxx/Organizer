04aa596068af91bbe45fbbe04c99255206fa1510
Mirafalxx <36042476+Mirafalxx@users.noreply.github.com>
2019-11-02 21:00:38 +0600

    add tasks to firebase ,and remove them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SelectorComponent } from "./selector/selector.component";
 import { OrganizerComponent } from "./organizer/organizer.component";
 import { MomentPipe } from "./shared/moment.pipe";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     OrganizerComponent,
     MomentPipe
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
